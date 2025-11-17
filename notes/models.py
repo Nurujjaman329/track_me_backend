59c61dbe9385121f3d58eebf9962819f9f86c9a9
Nurujjaman329 <mdnurujjaman329@gmail.com>
2025-11-17 10:35:30 +0600

    add priority part -> model

--- notes/models.py
+++ notes/models.py
@@ -8,6 +8,7 @@ class Note(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     days = models.JSONField(null=True, blank=True)   # store ["Mon", "Wed"]
     time = models.TimeField(null=True, blank=True)   # store scheduled time
+    priority = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return self.title
