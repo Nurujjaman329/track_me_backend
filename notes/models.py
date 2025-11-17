d4b4d1d047d21f3d34441b92ada5e1b5f72e5132
Nurujjaman329 <mdnurujjaman329@gmail.com>
2025-11-17 11:04:32 +0600

    remove day part and add date picked model

--- notes/models.py
+++ notes/models.py
@@ -6,8 +6,8 @@ class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    days = models.JSONField(null=True, blank=True)   # store ["Mon", "Wed"]
-    time = models.TimeField(null=True, blank=True)   # store scheduled time
+    date = models.DateField(null=True, blank=True)  # single date
+    time = models.TimeField(null=True, blank=True)
     priority = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
